55161c83440b6876b768ea8662afac2f9917c2e6
Bruno Matheus <bmatheusc@gmail.com>
2020-03-27 19:32:03 -0300

    Adding backend validation and tests

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,16 +13,30 @@ routes.post('/ongs', celebrate({
     [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10).max(11),
+        whatsapp: Joi.string().required().min(10).max(11),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2)
     })
 }), OngController.create); 
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required
+    }).unknown()
+}), ProfileController.index);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
 routes.post('/incidents', IncidentController.create); 
-routes.delete('/incidents/:id', IncidentController.delete); 
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete); 
 
 module.exports = routes;
